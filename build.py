656632c7f030a4d28e5963cabce3bf5d1c3c9d3a
baer1 <videocreator@outlook.de>
2024-10-15 19:52:58 +0200

    Update python script

--- build.py
+++ build.py
@@ -1,6 +1,14 @@
 import os
 import shutil
 
+from html.parser import HTMLParser
+
+
+class _HTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("HTML Tag:", tag, attrs, sep="\n  ")
+
+
 for dir, subdirs, files in os.walk("./www"):
     if (not subdirs and not files) or (
         all([file.startswith(".") for file in files])
@@ -13,3 +21,12 @@ for dir, subdirs, files in os.walk("./www"):
         if file.startswith(".") or file.endswith(".ts"):
             os.remove(os.path.join(dir, file))
             continue
+
+        if file.endswith(".html"):
+            try:
+                p = _HTMLParser()
+                p.feed(open(os.path.join(dir, file), "rb").read().decode())
+                p.close()
+                print(os.path.join(dir, file), "SUCCESS")
+            except Exception as e:
+                print(os.path.join(dir, file), e)
