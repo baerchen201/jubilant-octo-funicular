0a942dfdc5febda38823d75939a681962d43adf7
baer1 <videocreator@outlook.de>
2024-10-15 19:44:06 +0200

    Replace shell script with python script

---
+++ build.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+for dir, subdirs, files in os.walk("./www"):
+    if (not subdirs and not files) or (
+        all([file.startswith(".") for file in files])
+        and all([subdir.startswith(".") for subdir in subdirs])
+    ):
+        shutil.rmtree(dir)
+        continue
+
+    for file in files:
+        if file.startswith(".") or file.endswith(".ts"):
+            os.remove(file)
+            continue
