028624a4d96703014afafe4957520f70f6a002b2
baer1 <videocreator@outlook.de>
2024-10-25 15:35:33 +0200

    Update build.py

--- www/build.py
+++ www/build.py
@@ -14,48 +14,56 @@ class NavHTMLParser(HTMLParser):
         self.title = None
 
     def handle_starttag(self, tag, attrs):
-        print("  HTML Tag:", tag, attrs, sep="\n    ")
         if tag == "meta":
             for attr in attrs:
                 if attr[0] == "nav-title":
                     self.title = attr[1] if attr[1] else None
+                    print(f"     > Found HTML nav-title tag {self.title}")
 
 
 html_files = {}
 
 for dir, subdirs, files in os.walk("./www"):
+    print(
+        f"==> Processing directory {dir} ({len(subdirs)} {'subdirectory' if len(subdirs) == 1 else 'subdirectories'}, {len(files)} {'file' if len(files) == 1 else 'files'})"
+    )
+
     if (not subdirs and not files) or (
         all([file.startswith(".") for file in files])
         and all([subdir.startswith(".") for subdir in subdirs])
     ):
         shutil.rmtree(dir)
+        print(f"  > Removed directory tree {dir}")
         continue
 
     if os.path.isfile(os.path.join(dir, ".rm")):
+        print(f"  -> Found .rm file, processing...")
         for file in [
             os.path.join(dir, i.strip())
             for i in open(os.path.join(dir, ".rm"), "r").read().strip().splitlines()
         ]:
             if os.path.isdir(file):
                 shutil.rmtree(file)
+                print(f"     > Removed directory tree {file}")
             elif os.path.isfile(file):
                 os.remove(file)
+                print(f"     > Removed file {file}")
 
     for file in files:
         if file.startswith(".") or file.endswith(".ts"):
             os.remove(os.path.join(dir, file))
+            print(f"  > Removed file {os.path.join(dir, file)}")
             continue
 
         if file.endswith(".html"):
             try:
-                print(os.path.join(dir, file), "START")
+                print(f"  -> Processing HTML file {os.path.join(dir, file)}")
                 p = NavHTMLParser()
                 p.feed(open(os.path.join(dir, file), "rb").read().decode())
                 p.close()
                 html_files[os.path.join(dir[6:], file)] = p.title
-                print(os.path.join(dir, file), "SUCCESS")
             except Exception as e:
-                print(os.path.join(dir, file), e)
+                print(f"     > Failed ({e})")
 
 if html_files:
     with open("./www/nav.html", "wb") as f:
@@ -68,3 +76,6 @@ if html_files:
         f.write(
             f'<div style="margin-top:8px">{html_escape(sys.argv[-1])}</div></body></html>'.encode()
         )
+    print(
+        f"==> Generated nav.html file ({len(html_files.items())} {'item' if len(html_files.items()) == 1 else 'items'}, {os.path.getsize('./www/nav.html')} bytes)"
+    )
