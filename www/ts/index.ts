6241f87b51314dd5a8f668cf8362f7ffe7f6623c
baer1 <videocreator@outlook.de>
2024-11-20 11:24:00 +0100

    Update website

--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,14 +1,6 @@
-(
-  (
-    ((document as Document).querySelector as Function)(
-      "button" as string
-    ) as HTMLButtonElement
-  ).addEventListener as Function
-)(
-  "click" as string,
-  function func(event: MouseEvent): void | undefined | null {
-    ((window as Window).alert as Function)(
-      "Slightly over-typed TypeScript code" as string
-    ) as void;
-  } as Function
-) as void;
+(document.getElementById("button") as HTMLButtonElement).addEventListener(
+  "click",
+  function func(): void {
+    window.alert("Hello, World!");
+  }
+);
