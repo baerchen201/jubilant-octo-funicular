aaf6dc4d662f1ea8bf2540cb11b0232dc8b15e23
baer1 <videocreator@outlook.de>
2024-11-26 00:31:50 +0000

    Add status text modification to setdiscordstatus.py

--- linux/.config/swaylock/lock.sh
+++ linux/.config/swaylock/lock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Discord status
-python3 /usr/local/bin/setdiscordstatus.py -id "$(dirname $0)/discordstatus.json" -s idle
+python3 /usr/local/bin/setdiscordstatus.py -id "$(dirname $0)/discordstatus.json" -s idle -t "i use afk btw"
 
 # Lock screen
 swaylock -c "$(dirname $0)/config"
 
 # Reset discord status
-python3 /usr/local/bin/setdiscordstatus.py -ir "$(dirname $0)/discordstatus.json" -s online
+python3 /usr/local/bin/setdiscordstatus.py -ir "$(dirname $0)/discordstatus.json" -s online -t "i use arch btw"
