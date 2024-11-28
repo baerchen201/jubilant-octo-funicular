b0ecaee850b211ae1e1625c0e800217b74f948a5
baer1 <videocreator@outlook.de>
2024-11-28 02:03:17 +0100

    Fix lock screen
    not "fix" as in "fix", but it's less broken than before

--- linux/.config/swaylock/lock.sh
+++ linux/.config/swaylock/lock.sh
@@ -5,8 +5,8 @@ systemctl --user start discordidle.service &
 
 # Lock screen
 swaylock -c "$(dirname $0)/config"
-# Fallback if my dumbass didn't config correctly (idk i just found the idea cool)
-if ! [ $? = 0 ]; then waylock; fi
 
 # Reset discord status
 systemctl --user stop discordidle.service
+
+exit 0
