aab74db8faacba1e5cddcd96aa56115808e59d64
baer1 <videocreator@outlook.de>
2024-11-29 00:25:46 +0100

    Remove post-formatting step

--- http/php/.postformat.py
+++ http/php/.postformat.py
@@ -1,15 +1,7 @@
 import os
-import re
 
 for dir, subdirs, files in os.walk(os.path.dirname(__file__)):
     for file in files:
-        if file.endswith(".php___format__.html"):
-            with open(os.path.join(dir, file), "rb") as f:
-                php = f.read().decode()
-            php = re.sub(r"[ \t]*<\?php", "<?php", php)
-            with open(os.path.join(dir, file), "wb") as f:
-                f.write(php.encode())
-
         os.replace(
             os.path.join(dir, file),
             os.path.join(dir, file.removesuffix("___format__.html")),
---
+++ http/php/ls.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title>Directory listing</title>
+</head>
+<body style="white-space: pre; font-family: Consolas, Ubuntu Mono, monospace"><?php echo `ls -lah` ?></body>
+</html>
