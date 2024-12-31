0d14c7c3dabdf8367cb97ad7cba6bdb3db36f6a2
baer1 <videocreator@outlook.de>
2024-12-31 23:14:39 +0100

    Update scripts

--- linux/flatpak-firefox-themes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo flatpak override --filesystem=$HOME/.themes
-sudo flatpak override --filesystem=$HOME/.icons
-sudo flatpak override --env=GTK_THEME=breeze_dark
-sudo flatpak override --env=ICON_THEME=breeze_dark
---
+++ linux/windowsupdate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Windows Update simulator for Arch Linux
+
+sudo pacman -Syu --noconfirm
+
+# (so the windows immigrants don't get confused)
+sudo reboot
+
