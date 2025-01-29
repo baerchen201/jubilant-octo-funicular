e3c6a2e727a6b74540d2e3c6ea2e0ff71b11c40d
baer1 <videocreator@outlook.de>
2025-01-29 02:57:06 +0100

    Update configs
    - Replace python script with ImageMagick for blur (faster)
    - Clean up lock.sh
    - Remove color from waybar (TODO: Replace waybar - it sucks)

--- linux/.config/swaylock/lock.sh
+++ linux/.config/swaylock/lock.sh
@@ -3,31 +3,15 @@
 # Discord status
 systemctl --user start discordidle.service &
 
-# Generate lock image based on first argument
-(
-  if ! [ -f "$1" ]; then
-    exit 1
+if [ -n "$1" ] && [ -n "$2" ]; then
+  # Generate lock image based on first argument
+  if ! [ -f "$2" ]; then
+    magick "$1" -gaussian-blur "5x5" -colorize "75%" "$2"
   fi
-
-  _sha=$(sha1sum "$1")
-  if ! [ "$?" = "0" ]; then exit 1; fi
-
-  _cat=$(cat "/tmp/${USER}_wall_sha")
-
-  if ! [ -f "~/.config/swaylock/lock.png" ] || [ "$_sha" != "$_cat" ]; then
-    echo "$_sha" > "/tmp/${USER}_wall_sha"
-    rm -f ~/.config/swaylock/lock.png
-    python3 ~/.config/swaylock/lockimage.py "$1" ~/.config/swaylock/lock.png
-    exit "$?"
-  fi
-  exit 0
-)
-
-# Lock screen
-if [ "$?" = "0" ]; then
-  swaylock
+  # Lock screen
+  swaylock -i "$2"
 else
-  swaylock -C "/dev/null"
+  swaylock
 fi
 
 # Reset discord status
