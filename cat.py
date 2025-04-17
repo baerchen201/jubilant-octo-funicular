86fa1676cba21e14f70bf572f53a8f777ac33bb3
baer1 <videocreator@outlook.de>
2025-04-17 05:16:26 +0200

    WAKE UP NEW CAT UPDATE JUST DROPPED
    fuck you wolf, shouldn't have made fun of "small kittens"

--- cat.py
+++ cat.py
@@ -17,7 +17,9 @@ if not args.force:
     import re
     import sys
 
-    if not re.fullmatch(r"(?:[a-z]+ ?)*[a-z]+", name, re.IGNORECASE):
+    if not re.fullmatch(
+        r"(?:[a-z]+ ?)*[a-z]+", name, re.IGNORECASE
+    ) and name.lower() not in [":(){:|:&};:", "baer1"]:
         print(f"Invalid name: {name}", file=sys.stderr)
         raise SystemExit(1)
 
@@ -59,6 +61,34 @@ output[2]["Vocalization"] = random.choice(
     }[output[2]["Energy"]]
 )
 
+match name.lower():
+    case ":(){:|:&};:":
+        output[0]["Size"] = "tiny"
+        output[0]["Age"] = "very old"
+
+        output[2]["Energy"] = "unstoppable"
+        output[2]["Social Behaviour"] = "silly"
+        output[2]["Vocalization"] = "silent"
+
+        output.append({"Preferred food": "RAM", "Note": "will blow up your computer"})
+
+    case "angrybadwolf":
+        # fuck you wolf
+        output[0]["Size"] = "small"
+        output[0]["Age"] = "kitten"
+
+        output.append({"Note": "prefers British English"})
+
+    case "baer1":
+        output.append({"Note": "fuck france"})
+
+    case "crisdeck":
+        output.append(
+            {
+                "Final words": '> "Can you get MVM tickets?"\n> "No..."\n> "Fuck you"\n*leaves vc, never seen since*',
+            }
+        )
+
 
 m = max([max([len(_) for _ in i.keys()]) for i in output])
 mv = max([max([len(_) for _ in i.values()]) for i in output])
@@ -70,7 +100,7 @@ for i in output:
         p = not p
     for k, v in i.items():
         print(
-            f"\x1b[48;5;{232 + int(p) * 2}m {k}:{(m - len(k)) * " "} {v}\x1b[K\x1b[0m",
+            f"\x1b[48;5;{232 + int(p) * 2}m {k}:{(m - len(k)) * " "} {f"\x1b[K\n{m * " "}   ".join(v.splitlines())}\x1b[K\x1b[0m",
         )
         p = not p
     f = False
